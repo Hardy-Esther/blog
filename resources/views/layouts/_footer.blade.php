293b5e621b4a23a4cb337a9f155a6689e32cc9c2
ZWJ <1176566869@qq.com>
2019-11-22 18:46:59 +0800

    共同页面

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<!--页尾-->
+<footer class="footer">
+    <div class="container">
+        <p class="text-center">
+            Copyright © 2019 Hardy'Blog
+        </p>
+    </div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,21 @@
+<!--页头-->
+<div class="header">
+    <div class="header-bg">
+        <div class="blog-title">
+            <div class="container">
+                <h1>Hardy'Blog</h1>
+                <h5>每个人都在寻找自己；少部分的人找到了，大部分的人还在寻找的路上</h5>
+            </div>
+        </div>
+    </div>
+    <nav class="navbar-expand-lg bg-black">
+        <div class="container">
+            <ul id="nav-list">
+                <li class="nav-item"><a href="index.html">首页</a></li>
+                <li class="nav-item"><a href="javascript:;">编程</a></li>
+                <li class="nav-item"><a href="javascript:;">算法</a></li>
+                <li class="nav-item"><a href="javascript:;">留言</a></li>
+            </ul>
+        </div>
+    </nav>
+</div>
