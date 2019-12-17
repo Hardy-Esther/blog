543aab2de689d45cbdb6186208121ae5fd93b5fd
ZWJ <1176566869@qq.com>
2019-12-17 15:44:06 +0800

    添加live2d看板娘

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,10 @@
     <meta name="keyword" content="@yield('keyword', "Hardy'Blog 编程、算法学习分享")"/>
     <!-- Styles -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+
+    <!-- 引用liv2D -->
+    <link rel="stylesheet" type="text/css" href="{{asset("live2d/assets/waifu.css")}}" />
+
     @yield('styles')
 </head>
 
@@ -34,8 +38,30 @@
     @include('layouts._footer')
 </div>
 
+<!-- live2D -->
+<div class="waifu">
+    <div class="waifu-tips"></div>
+    <canvas id="live2d" width="280" height="300" class="live2d"></canvas>
+    <div class="waifu-tool">
+        <span class="fui-home"></span>
+        <span class="fui-chat"></span>
+        <span class="fui-eye"></span>
+        <span class="fui-user"></span>
+        <span class="fui-photo"></span>
+        <span class="fui-info-circle"></span>
+        <span class="fui-cross"></span>
+    </div>
+</div>
+
 <!-- Scripts -->
 <script src="{{ mix('js/app.js') }}"></script>
+<!-- 引用liv2D -->
+<script src="{{asset("live2d/assets/waifu-tips.js")}}"></script>
+<script src="{{asset("live2d/assets/live2d.js")}}"></script>
+<script type="text/javascript">
+    initModel("{{asset("live2d/assets/")}}");
+</script>
+
 @yield("scripts")
 <script>
     $(document).ready(function () {
